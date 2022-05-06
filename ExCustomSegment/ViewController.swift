88c83dfb6671664be7800cdeb04050447270cad1
jake.k <jake.k@hpcnt.com>
2022-05-06 10:05:03 +0900

    Initial Commit

---
+++ ExCustomSegment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExCustomSegment
+//
+//  Created by Jake.K on 2022/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
